1339a38775106e9c06903be8182fc554d7d0778c
John Grinwis <john.grinwis@gmail.com>
2022-02-07 19:30:39 +0100

    test with sensitive = true

--- environments/staging/outputs.tf
+++ environments/staging/outputs.tf
@@ -1,4 +1,5 @@
 # output the created policy id
 output "active_version" {
-  value = resource.akamai_cloudlets_policy_activation.pr_staging.version
+  value     = resource.akamai_cloudlets_policy_activation.pr_staging.version
+  sensitive = true
 }
