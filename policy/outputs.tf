9dab33a169e2808e05f607068b4f31e05b49f8a0
John Grinwis <john.grinwis@gmail.com>
2022-02-07 18:07:36 +0100

    added extra directory in policy

--- policy/outputs.tf
+++ policy/outputs.tf
@@ -3,6 +3,7 @@ output "id" {
   value = resource.akamai_cloudlets_policy.phased_release.id
 }
 
+# little trick to lookup the latest version
 output "latest_version" {
   value = split(":", data.akamai_cloudlets_policy.pr_policy.id)[1]
 }
