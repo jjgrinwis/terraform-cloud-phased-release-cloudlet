99a17e22627049bb08f271321d1e2d26e60794bd
John Grinwis <john.grinwis@gmail.com>
2022-02-07 19:40:55 +0100

    move to non-sensative values

--- environments/staging/outputs.tf
+++ environments/staging/outputs.tf
@@ -1,4 +1,4 @@
 # output the created policy id
 output "active_version" {
-  value = var.policy_version == null ? nonsensative(data.tfe_outputs.policy.values["latest_version"]) : var.policy_version
+  value = var.policy_version == null ? nonsensitive(data.tfe_outputs.policy.values["latest_version"]) : var.policy_version
 }
